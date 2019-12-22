9967d73cd81128e80f88c4b831ae10eb1bfcc849
TJ Hardin <MrT246007@gmail.com>
2019-12-22 03:27:52 -0600

    Setting up Redux

---
+++ src/Store/reducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+    persons: [{testAge: 23, testID: 5, testName: 'TJ'}]
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+}
+
+export default reducer;
--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
@@ -41,4 +42,10 @@ class Persons extends Component {
     }
 }
 
-export default Persons;
+const mapStateToProps = state => {
+    return {
+        person: state.persons
+    };
+};
+
+export default connect(mapStateToProps)(Persons);
